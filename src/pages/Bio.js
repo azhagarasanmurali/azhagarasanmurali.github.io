9690f97ae763686696eaeb3e133a8b5046ae1a5a
Azhagarasan <azhagarasan.azhagu@gmail.com>
2025-06-30 22:28:18 +0530

    Update Bio.js

--- src/pages/Bio.js
+++ src/pages/Bio.js
@@ -4,6 +4,25 @@ const Bio = () => (
 	<div>
 		<h1>Bio</h1>
 		<h3>Detailed biography and background.</h3>
+		<h1>Strengths</h1>
+		<p>
+			<ul>
+				<b>Architect Mindset:</b> Breaks down systems into primitives.
+				Builds from zero where others iterate from templates.
+			</ul>
+			<ul>
+				<b>Recursive Learner:</b> Discards prior knowledge when
+				obsolete, reboots skill paths independently.
+			</ul>
+			<ul>
+				Self-debug my cognition, removing inefficiencies without
+				external pressure.
+			</ul>
+			<ul>
+				<b>Scalablity:</b> Build systems that scales, not just to solve
+				tasks.{" "}
+			</ul>
+		</p>
 	</div>
 );
 
