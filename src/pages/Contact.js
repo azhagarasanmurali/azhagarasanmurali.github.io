17ed7909d70de04e917730f8846d1138ad66741d
Azhagarasan <azhagarasan.azhagu@gmail.com>
2025-08-06 11:44:48 +0530

    Mail hyperlink added.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,7 @@ import React from "react";
 const Contact = () => (
 	<div>
 		<h1>Contact</h1>
-		<h3>Email: azhagarasan.azhagu@gmail.com</h3>
+		<h3>Email: <a href="mailto:azhagarasan.azhagu@gmail.com">azhagarasan.azhagu@gmail.com</a></h3>
 	</div>
 );
 
