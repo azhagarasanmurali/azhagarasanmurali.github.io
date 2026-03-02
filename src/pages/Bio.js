9ba20a66ca2c790f7cd23a9c331d92d0ae5448ef
Azhagarasan Murali <azhagarasan.azhagu@gmail.com>
2026-03-02 11:35:59 +0530

    added work experience in bio page

--- src/pages/Bio.js
+++ src/pages/Bio.js
@@ -3,7 +3,66 @@ import React from "react";
 const Bio = () => (
 	<div>
 		<h1>Bio</h1>
-		<h3>Detailed biography and background.</h3>
+
+		<h1>Career Experience</h1>
+		<p>
+			<ul>
+				<b font-size="large">Unity Programmer</b>
+				<tab />
+				<i>(Sep 2020 - Present)</i>
+				<br />
+				<tab /> <b>Sify Technologies</b> - Tidel Park, Chennai
+			</ul>
+			<ul>
+				<b font-size="large">Unity Game Developer Intern</b>
+				<tab />
+				<i>(Dec 2019 - Feb 2020)</i>
+				<br />
+				<tab /> <b>Gamin Inztinct</b> - Mylapore, Chennai
+			</ul>
+			<ul>
+				<b font-size="large">J2EE Back End Web Developer</b>
+				<tab />
+				<i>(Oct 2017 - Aug 2018)</i>
+				<br />
+				<tab /> <b>Wipro Technologies</b> - Sholinganallur, Chennai
+			</ul>
+		</p>
+
+		<h1>Education</h1>
+		<p>
+			<ul>
+				<b font-size="large">M.Sc. Game Technology</b>
+				<tab />
+				<i>2020 (CGPA: 69.82)</i>
+				<br />
+				<tab /> <b>ICAT Design & Media College</b> - Mylapore, Chennai
+			</ul>
+			<ul>
+				<b font-size="large">B.E. Computer Science</b>
+				<tab />
+				<i>2017 (CGPA: 8.12)</i>
+				<br />
+				<tab /> <b>M.Kumarasamy College of Engineering</b> -
+				Thalavapalayam, Karur
+			</ul>
+			<ul>
+				<b font-size="large">Higher Secondary</b>
+				<tab />
+				<i>2013 (85.6%)</i>
+				<br />
+				<tab /> <b>Sowdambikaa Matric Higher Secondary School</b> -
+				Thuraiyur, Tiruchchirappalli
+			</ul>
+			<ul>
+				<b font-size="large">SSLC</b>
+				<tab />
+				<i>2011 (89.2%)</i>
+				<br />
+				<tab /> <b>Raghavendra Matriculation School</b> - Thuraiyur,
+				Tiruchchirappalli
+			</ul>
+		</p>
 
 		<h1>Strengths</h1>
 		<p>
